e60e03b069b9e34b68a5d87108f0e8cbbcfeb995
Mohammed Raafat <moohamedraafat@gmail.com>
2021-10-11 00:59:15 +0200

    test

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,16 @@
-// import React, { useState, useEffect, useRef } from "react";
-// import { Link, NavLink } from "react-router-dom";
+import React, { useState, useEffect, useRef } from "react";
+import { Link, NavLink } from "react-router-dom";
 
-// import { Grid, Divider, Hidden, IconButton, Box, AppBar } from "@mui/material";
+import { Grid, Divider, Hidden, IconButton, Box, AppBar } from "@mui/material";
 // import MenuIcon from "@mui/icons-material/Menu";
 
 // // import SmallDeviceMenu from "./SmallDeviceMenu";
-// import LANGUAGE from "../../shared/localization/language";
+import LANGUAGE from "../../shared/localization/language";
 
 // const bostaLogo = require(`../../assets/images/bosta_logo_${
 //   LANGUAGE.language === "ar" ? "ar" : "en"
 // }.svg`).default;
 
-import React from "react";
 
 const Navbar = () => {
   /* const [openMenu, setOpenMenu] = useState(false);
