ea640a33bb8a4cb358afca6ee4e9466fc1d986ad
mohamed.raafat <mohamed.rafaat@tooli.tv>
2021-10-11 18:58:44 +0200

    test

--- src/shared/localization/language.js
+++ src/shared/localization/language.js
@@ -6,6 +6,7 @@ const lang = {
   en: english
 };
 
-const LANGUAGE = lang.en; //lang[localStorage.getItem("userLanguage") || "en"];
+// const LANGUAGE = lang.en; //lang[localStorage.getItem("userLanguage") || "en"];
+const LANGUAGE = localStorage.getItem("userLanguage")? lang[localStorage.getItem("userLanguage")] : lang.en;
 
 export default LANGUAGE;
