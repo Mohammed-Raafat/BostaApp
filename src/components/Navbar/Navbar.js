3e026b07c375aee1c5ae37b9c4284ef1fe0fc3d7
mohamed.raafat <mohamed.rafaat@tooli.tv>
2021-10-11 13:02:06 +0200

    test

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,7 +7,7 @@ import React from "react";
 // import MenuIcon from "@mui/icons-material/Menu";
 
 // // import SmallDeviceMenu from "./SmallDeviceMenu";
-import LANGUAGE from "../../shared/localization/language";
+// import LANGUAGE from "../../shared/localization/language";
 
 // const bostaLogo = require(`../../assets/images/bosta_logo_${
 //   LANGUAGE.language === "ar" ? "ar" : "en"
