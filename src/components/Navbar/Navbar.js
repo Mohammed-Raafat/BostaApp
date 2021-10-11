1da2e111ca62877b8d0174df7e4541cfe7356006
mohamed.raafat <mohamed.rafaat@tooli.tv>
2021-10-11 17:45:34 +0200

    test

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,17 @@
 import React, { useState, useEffect, useRef } from "react";
 // import React from "react";
 
-import { Link, NavLink } from "react-router-dom";
+// import { Link, NavLink } from "react-router-dom";
 
-import { Grid, Divider, Hidden, IconButton, Box, AppBar } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
+// import { Grid, Divider, Hidden, IconButton, Box, AppBar } from "@mui/material";
+// import MenuIcon from "@mui/icons-material/Menu";
 
 // import SmallDeviceMenu from "./SmallDeviceMenu";
-import LANGUAGE from "../../shared/localization/language";
+// import LANGUAGE from "../../shared/localization/language";
 
-const bostaLogo = require(`../../assets/images/bosta_logo_${
-  LANGUAGE.language === "ar" ? "ar" : "en"
-}.svg`).default;
+// const bostaLogo = require(`../../assets/images/bosta_logo_${
+//   LANGUAGE.language === "ar" ? "ar" : "en"
+// }.svg`).default;
 
 
 const Navbar = () => {
