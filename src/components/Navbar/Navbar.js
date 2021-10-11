fcfe0d406e26244c8c23b3262d9080bdde5a1f2f
mohamed.raafat <mohamed.rafaat@tooli.tv>
2021-10-11 18:35:32 +0200

    test

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,14 +7,15 @@ import { Grid, Divider, Hidden, IconButton, Box, AppBar } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 
 // import SmallDeviceMenu from "./SmallDeviceMenu";
-// import LANGUAGE from "../../shared/localization/language";
+import LANGUAGE from "../../shared/localization/language";
 
-const bostaLogo = require(`../../assets/images/bosta_logo_${
-  "en"// LANGUAGE.language === "ar" ? "ar" : "en"
-}.svg`).default;
+
 
 
 const Navbar = () => {
+  const bostaLogo = require(`../../assets/images/bosta_logo_${
+    LANGUAGE.language === "ar" ? "ar" : "en"
+  }.svg`).default;
   //  const [openMenu, setOpenMenu] = React.useState(false);
 
   // const navbarRef = useRef();
