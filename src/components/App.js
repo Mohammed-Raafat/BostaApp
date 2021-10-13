5a41af9651ca841be3689b471bd8b1466828c5b8
Mohammed Raafat <moohamedraafat@gmail.com>
2021-10-13 10:54:00 +0200

    test navegation

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,7 @@ import ShipmentTracking from "./ShipmentTracking/ShipmentTracking";
 import Footer from "./Footer";
 
 import LANGUAGE from "../shared/localization/language";
+import Test from './Test';
 
 // Create rtl cache
 const cacheRtl = createCache({
@@ -52,6 +53,7 @@ const App = () => {
                 exact
                 component={ShipmentTracking}
               />
+              <Route path="/prices" exact component={Test} />
               <Route path="/" exact component={Home} />
               <Route render={() => <Redirect to="/" />} />
             </Switch>
